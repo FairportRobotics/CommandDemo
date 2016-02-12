ca429992da353e0f73a9a17d8e93f1ae932a0186
wildog4 <wildog4@hotmail.com>
2016-02-11 19:47:32 -0500

    edited the autonomus turn to zero command

--- src/org/usfirst/frc/team578/robot/commands/autonomous/AutonomousTurnToZero.java
+++ src/org/usfirst/frc/team578/robot/commands/autonomous/AutonomousTurnToZero.java
@@ -1,8 +1,14 @@
 package org.usfirst.frc.team578.robot.commands.autonomous;
 
+import org.usfirst.frc.team578.robot.Robot;
+
 import edu.wpi.first.wpilibj.command.Command;
 
 public class AutonomousTurnToZero extends Command{
+	
+	public AutonomousTurnToZero() {
+		requires(Robot.driveSubsystem);
+	}
 
 	@Override
 	protected void initialize() {
@@ -13,7 +19,14 @@ public class AutonomousTurnToZero extends Command{
 	@Override
 	protected void execute() {
 		// TODO Auto-generated method stub
-		
+		if(Robot.navx.getFusedHeading()>358||Robot.navx.getFusedHeading()<2)
+		{
+			Robot.driveSubsystem.driveMotors(0, 0, 0, 0);
+		}
+		else
+		{
+			Robot.driveSubsystem.driveMotors(.2, -.2, .2, -.2);
+		}
 	}
 
 	@Override
